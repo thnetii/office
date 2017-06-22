5d95d29ab5e5227151a3c0a4e2982b1e9915a0f9
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-06-22 12:50:32 +0200

    Add empty Office-to-Git Word Plugin Project

---
+++ src/THNETII.Office.Office2Git.WordPlugin/Office2GitWordAddin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace THNETII.Office.Office2Git.WordPlugin
+{
+    public partial class Office2GitWordAddin
+    {
+        private void WordAddinStartup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void WordAddinnShutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(WordAddinStartup);
+            this.Shutdown += new System.EventHandler(WordAddinnShutdown);
+        }
+        
+        #endregion
+    }
+}
---
+++ src/THNETII.Office.Office2Git.WordPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("THNETII.Office.Office2Git.WordPlugin")]
+[assembly: AssemblyDescription("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else // !DEBUG
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("TH-NETII Rasch Solutions")]
+[assembly: AssemblyProduct("THNETII.Office.Office2Git.WordPlugin")]
+[assembly: AssemblyCopyright("Copyright © TH-NETII Rasch Solutions 2017")]
+[assembly: AssemblyTrademark("Written by Fredrik Høisæther Rasch")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5db9f72d-028e-4459-950e-edd19feb9de0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("15.0.4937.1000")]
+[assembly: AssemblyFileVersion("15.0.4937.1000")]
+
